e37ca66071b41f6610e0dd970758f4f4ef746ee0
Sasmita <sasmi.sashi@gmail.com>
2013-11-19 20:50:23 -0800

    Added first version of Programming LEDs to different temperature conditions

---
+++ serial_led.ino
@@ -0,0 +1,33 @@
+
+/*
+
+QuantumLight
+SISINTY SASMITA PATRA
+
+This program lit the led if the temp is high and 
+it turns off the led if temp is low
+
+*/
+
+int pin = 13;
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(pin, OUTPUT);
+}
+
+void loop()
+{
+  while (Serial.available() > 0) {
+    int input = Serial.parseInt(); 
+    if(input == 1)
+    {
+      digitalWrite(pin, HIGH); //if temp is high led is on
+    }
+    else
+    {
+      digitalWrite(pin, LOW);//If temp is low, led is off
+    }
+  }
+}
