12e7f3c5af4976223710d66f7edcf2c7e8eb630d
Sasmita <sasmi.sashi@gmail.com>
2013-11-22 16:55:16 -0800

    updated file

--- quantumLight.py
+++ quantumLight.py
@@ -1,3 +1,17 @@
+/*
+
+QuantumLight
+
+SISINTY SASMITA PATRA
+SMITA KUNDARGI
+JEANNE VICTOR
+
+FileName: quantumLight.py
+
+This program also calls the getWeatherclassification module from classify.py. This program communicates from computer to Arduino and turns the LED on/off depending on the classification obtained from a particular weather condition. 
+
+*/
+
 from classify import getWeatherClassification
 import serial
 
