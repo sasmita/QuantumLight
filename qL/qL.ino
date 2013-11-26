00937c29fee9c757234284f33dc8614120fa5080
Sasmita <sasmi.sashi@gmail.com>
2013-11-26 07:56:55 -0800

    Initial version of displaying LEDs

---
+++ qL/qL.ino
@@ -0,0 +1,29 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, strip.Color(0,0,0));
+  }
+  
+  strip.setPixelColor(30, strip.Color(0,255,0));
+  
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+
+}
+
