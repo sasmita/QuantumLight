2e30cf61f7f737bbb0f3661bef84202b3901eac2
Betcy <cse85bets@gmail.com>
2013-12-13 14:07:32 -0800

    Updated weather.py
    Updated weather.py to include new function that retrieves & adds weather info to the csv file

--- weather.py
+++ weather.py
@@ -18,6 +18,11 @@ weatherResult = pywapi.get_weather_from_weather_com(zipcode)
 
 import pywapi
 import string
+import csv
+
+zipcode = '97006'
+zipcodeAlas = '99501'
+zipcodeLV = '89110'
 
 """
 Funtion that retrieves weather information for the given zipcode;
@@ -28,9 +33,38 @@ def getWeatherInfo(zipcode):
     weatherList = []
     result = pywapi.get_weather_from_yahoo(zipcode)
     weatherList = [float(result['condition']['temp']), 
-		   float(result['atmosphere']['humidity']), 
+                   float(result['atmosphere']['humidity']), 
                    float(result['wind']['speed'])
                   ]
 
     return weatherList
-    
+
+"""
+Funtion that retrieves weather information and appends the data to the CSV file;
+
+Params:
+zipcode - zipcode of the city for which weather info has to be retrieved
+filename - the csv filename to which the data has to be appended
+"""
+
+def getWeatherInfoInCSV(zipcode, filename):
+    wList = []
+    result = pywapi.get_weather_from_yahoo(zipcode)
+    wList = [result['condition']['title'],
+                   result['condition']['date'],
+                   int(result['condition']['temp']),
+                   float(result['atmosphere']['humidity']),
+                   int(result['wind']['chill']),
+                   int(result['wind']['direction']),
+                   float(result['wind']['speed']) ]
+    fd = open('%s.csv' % filename,'ab')
+    #fd = open('SampleData.csv','ab')
+    writer = csv.writer(fd)
+    writer.writerow((wList)) 
+    fd.close()
+    return wList
+
+
+
+getWeatherInfoInCSV(zipcode,"SampleDataForOR")
+
